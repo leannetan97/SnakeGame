13a4ac7212e21f87827b836466d845e8d32d670b
Nabilah Husna <nabilah97@gmail.com>
2019-10-11 10:16:02 +0800

    Add Record class
    Added getName(), writeToFile(), readFromFile() but only name, date & time

--- src/maintenanceassignment/MaintenanceAssignment.java
+++ src/maintenanceassignment/MaintenanceAssignment.java
@@ -5,6 +5,15 @@
  */
 package maintenanceassignment;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 /**
  *
  * @author Leanne
@@ -35,9 +44,9 @@ class Coordinate{
     }
     
 }
-class Snake{
-    Coordinate
-}
+//class Snake{ 
+//    Coordinate
+//}
 
 
 public class MaintenanceAssignment {
@@ -46,7 +55,64 @@ public class MaintenanceAssignment {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    
+    Record user = new Record();
+    user.writeToFile();
+    user.readFromFile();
+    
     }
     
+}
+
+class Record{
+    String name;
+    String capName;
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+ 
+
+    
+    public String getName(){
+        System.out.println("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine().toString();
+        capName = name.substring(0,1).toUpperCase() + name.substring(1);
+        return capName;
+    }
+    
+    public void readFromFile(){
+        
+        BufferedReader reader;
+        
+        try{
+        reader = new BufferedReader(new FileReader("\\Users\\user\\Documents\\GitHub\\SnakeGame\\record.txt"));
+        String line = reader.readLine();
+            System.out.println("Reading File...");
+        while (line!=null){
+            System.out.println(line);
+            line = reader.readLine();
+        }
+        reader.close();
+        }catch(IOException e){
+            System.out.println("ERROR.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    
+    public void writeToFile() {
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyy/MM/dd HH:mm:ss");
+//        LocalDateTime now = LocalDateTime.now();
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter("\\Users\\user\\Documents\\GitHub\\SnakeGame\\record.txt", true));
+            writer.println("[" + dtf.format(now) + "] Your name: " + getName());
+            writer.close();
+            System.out.println("Name saved.");
+        } catch(IOException e){
+            System.out.println("ERROR.");
+            e.printStackTrace();
+            System.exit(1);
+}
+    }
 }
